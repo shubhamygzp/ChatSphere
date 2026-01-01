fbc7da75420476b372bcf6983947c89609ed18fc
Shubham Yadav <shubhamgzpyadav@gmail.com>
2026-01-01 14:53:09 +0530

    Formatted Time added to each message

--- frontend/src/home/RightPart/Message.jsx
+++ frontend/src/home/RightPart/Message.jsx
@@ -7,6 +7,12 @@ function Message({ message }) {
   const chatName = itsMe ? " chat-end" : "chat-start";
   const chatColor = itsMe ? "bg-blue-500" : "";
 
+  const createdAt = new Date(message.createdAt);
+  const formattedTime = createdAt.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
   return (
     <div>
       <div className="p-4">
@@ -14,6 +20,8 @@ function Message({ message }) {
           <div className={`chat-bubble text-white ${chatColor}`}>
             {message.message}
           </div>
+          <div className="chat-footer">{
+            formattedTime}</div>
         </div>
       </div>
     </div>
