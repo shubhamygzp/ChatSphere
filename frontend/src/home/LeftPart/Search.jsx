e0444065851ccbad84bfe8b79ba0ec3e90ea56da
Shubham Yadav <shubhamgzpyadav@gmail.com>
2026-01-01 14:41:02 +0530

    Search Functionality implemented

--- frontend/src/home/LeftPart/Search.jsx
+++ frontend/src/home/LeftPart/Search.jsx
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import useGetAllUsers from "../../context/useGetAllUsers";
+import useConversation from "../../zustand/useConversation";
 
 function Search() {
+  const [search, setSearch] = useState("");
+  const [allUsers] = useGetAllUsers();
+  const { setSelectedConversation } = useConversation();
+  console.log(allUsers);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    const conversation = allUsers.find((user) =>
+      user.fullName?.toLowerCase().includes(search.toLowerCase())
+    );
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else {
+      alert("User not found");
+    }
+  };
   return (
     <div className="h-[10vh]">
       <div className="px-6 py-4">
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className="flex space-x-3">
             <label className="border border-gray-700 bg-slate-900 rounded-lg p-3 flex items-center gap-2 w-[80%]">
               <input
                 type="text"
                 className="grow outline-none bg-transparent"
                 placeholder="Search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
               />
             </label>
             <button>
